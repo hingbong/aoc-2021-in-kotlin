89d83325872b1fe3c132bf0862b1a02758f26f41
hingbong <hingbonglo@gmail.com>
2021-12-01 14:26:30 +0800

    submit day01 part2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,19 +1,51 @@
 fun main() {
-    var counter = 0
     val input = readInput("Day01")
-    if (input.size < 2) {
-        println(0)
-        return
-    }
-    var previous = input[0].toInt()
-    (1 until input.size)
-        .map { input[it] }
-        .forEach {
-            val i = it.toInt()
-            if (i > previous) {
-                counter++
-            }
-            previous = i
+    run part01@{
+        var counter = 0
+        if (input.size < 2) {
+            println(0)
+            return
         }
-    println(counter)
+        var previous = input[0].toInt()
+        (1 until input.size)
+            .map { input[it] }
+            .forEach {
+                val i = it.toInt()
+                if (i > previous) {
+                    counter++
+                }
+                previous = i
+            }
+        println(counter)
+    }
+    run part02@{
+        var counter = 0
+        val list = object : ArrayList<Int>(MAX_SIZE) {
+            override fun add(element: Int): Boolean {
+                if (size == MAX_SIZE) {
+                    removeFirst()
+                }
+                return super.add(element)
+            }
+        }
+        if (input.size < MAX_SIZE) {
+            println(0)
+            return
+        }
+        list.add(input[0].toInt())
+        list.add(input[1].toInt())
+        list.add(input[2].toInt())
+        (MAX_SIZE until input.size)
+            .forEach {
+                val i = input[it].toInt()
+                val previous = list.sum()
+                list.add(i)
+                if (previous < list.sum()) {
+                    counter++
+                }
+            }
+        println(counter)
+    }
 }
+
+private const val MAX_SIZE = 3
